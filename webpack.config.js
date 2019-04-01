2bed7c9df15dc26f8da4755d3d8c5fa3e010c2a3
Imran Sayed <imranhsayed@gmail.com>
2019-04-01 13:32:18 +0530

    Update the default config of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const defaultConfig = require("./node_modules/@wordpress/scripts/config/webpack.config");
+const path = require( 'path' );
+console.log( defaultConfig.externals );
+// console.log( path.resolve( __dirname, 'bundle' ) );
 
 module.exports = {
 	...defaultConfig,
+	mode: 'development',
+	context: __dirname,
+	entry:
+		{
+			index: './src/index.js'
+		},
+	output:
+		{ filename: '[name].js',
+			path: path.resolve( __dirname, 'bundle' ),
+		},
 	module: {
-		...defaultConfig.module,
-		rules: [
-			...defaultConfig.module.rules,
-			{
-				test: /\.svg$/,
-				use: ["@svgr/webpack", "url-loader"]
-			}
-		]
+		...defaultConfig.module
 	}
 };
