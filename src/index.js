a54b03a74a5322d328a4c1b138719aea0b84138c
Imran Sayed <imranhsayed@gmail.com>
2019-04-01 12:14:25 +0530

    Add script to fix javascript errors

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ registerBlockType( 'myguten-block/test-block', {
 	icon: 'smiley',
 	category: 'layout',
 	edit: ( { className } ) => <div className={ className }>Hello World!</div>,
-	save: () => <div>Hello World!</div>,
+	save: () => <div>Hello World!</div>
 } );
