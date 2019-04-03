eec91506f43530dff448308cacbb449e8f8c9d4c
Imran Sayed <imranhsayed@gmail.com>
2019-04-03 12:22:25 +0530

    Add alignmentToolbar and BlockControls

--- src/index.js
+++ src/index.js
@@ -1,11 +1,58 @@
-
-// src/index.js
 const { registerBlockType } = wp.blocks;
+const { RichText, BlockControls, AlignmentToolbar } = wp.editor;
 
-registerBlockType( 'myguten-block/test-block', {
+const result = registerBlockType( 'myguten-block/test-block', {
 	title: 'Basic Example',
 	icon: 'smiley',
 	category: 'layout',
-	edit: ( { className } ) => <div className={ className }>Hello World!</div>,
-	save: () => <div>Hello World!</div>
+	attributes: {
+		content: {
+			type: 'array',
+			source: 'children',
+			selector: 'p',
+		},
+	},
+	edit: ( props ) => {
+		console.log( props );
+		const { attributes: { content, alignment }, setAttributes, className } = props;
+		const onChangeContent = ( newContent ) => {
+			setAttributes( { content: newContent } );
+		};
+
+		const onChangeAlignment = ( newAlignment ) => {
+			let alignmentValue = ( newAlignment === undefined ) ? 'none' : newAlignment;
+				setAttributes( { alignment: alignmentValue } )
+		};
+
+		return (
+			<div>
+				{
+					<BlockControls>
+						<AlignmentToolbar
+							value={ alignment }
+							onChange={ onChangeAlignment }
+						/>
+					</BlockControls>
+				}
+				<RichText
+					tagName="p"
+					style = {{ textAlign: alignment }}
+					className={ className }
+					onChange={ onChangeContent }
+					value={ content }
+				/>
+			</div>
+		);
+	},
+	save: ( props ) => {
+		return (
+			<RichText.Content
+				className={ `myguten-block-align-${ props.attributes.alignment }` }
+				tagName="p"
+				value={ props.attributes.content }
+			/>
+		);
+	},
 } );
+
+console.log( result );
